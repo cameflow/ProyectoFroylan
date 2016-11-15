fa8e56f6456c494c82b82599a95463b7f7473758
cameflow <alex.terrazas.z@gmail.com>
2016-11-14 20:42:46 -0600

    :octocat: Frist Commit

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityRandom urand = new UnityRandom ();
+		float val = urand.Possion (035.6f);
+		float val2 = urand.Possion (3.0f);
+		Debug.Log (val);
+		Debug.Log (val2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
